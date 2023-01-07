78b6dcb0ed8095c034dd3872ead53aaf94c09d43
Amit Singh Chouhan <amitchouhan948@gmail.com>
2023-01-07 13:27:20 +0530

    backend route with pagination done feature added in api

--- cointab_assign_backend/src/Routes/Users.route.js
+++ cointab_assign_backend/src/Routes/Users.route.js
@@ -4,13 +4,15 @@ const Users= require("../Modal/Users.modal")
 
 const app=express.Router();
 
+// To store users in mongoDB (fetch users button command-"BEGIN")
+
 app.get("/",async(req,res)=>{
 
     try{
         
         const data= await axios.get("https://randomuser.me/api/?page=1&results=50")
 
-        Users.collection.remove()
+        Users.collection.deleteMany()
 
         let userData= await Users.create(data.data.results);
 
@@ -31,7 +33,37 @@ app.get("/",async(req,res)=>{
 
 });
 
+// To store users in mongoDB (fetch users button command-"END")
 
+/////////////////////////////// --------- --------- /////////////////////////////////
+
+// To get data for frontend from mongoDB (User detail button command with pagination feature-"BEGIN")
+
+app.get("/details",async(req,res)=>{
+
+        let page=req.query.page
+
+    try{
+
+        console.log(page)
+
+        let data= await Users.find().skip(page*10-10).limit(10);
+
+        // console.log("x:",x)
+
+        res.send(data);
+
+      }
+      catch(err){
+      
+        res.send(err)
+      }
+
+
+});
+
+
+// To get data for frontend from mongoDB (User detail button command with pagination feature-"END")
 
 
 module.exports=app;
